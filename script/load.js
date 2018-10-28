8f59cf75c7de3b9dbd35395608782daf1ad1d77e
Ching2003 <44335786+Ching2003@users.noreply.github.com>
2018-10-28 10:52:39 +0800

    Update script/load.js

--- script/load.js
+++ script/load.js
@@ -2,7 +2,7 @@ function load () {
 	var loadBoard = this;  // 取得loadBoard
 }
 window.onload = function(){
-	loadBoard.childNodes[1].style.opacity = "1";  // 取得loadBoard中的元素
+	loadBoard.childNodes[1].style.opacity = "1";  // 顯示出loadBoard中的元素
 	setTimeout(function(){  // 等三秒 transition 特效結束
 		loadBoard.style.opacity = "0";
 		setTimeout(function(){  // 等三秒 transition 特效結束
