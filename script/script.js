4a099392d94585a65a58bab592637f1c0e5c5a88
Ching <44335786+Ching367436@users.noreply.github.com>
2019-01-26 07:07:38 +0800

    add end

--- script/script.js
+++ script/script.js
@@ -3,17 +3,17 @@ function o (a) {
 	return (a < 10) ? "0" + a : a;
 }
 
-c(new Date(2019, 4, 18, 8, 20).getTime(), '會考');
-c(new Date(2019, 0, 25, 9, 15).getTime(), '學測');
+c(new Date(2019, 4, 18, 8, 20).getTime(), '會考', new Date(2019, 4, 19, 12, 50).getTime());
+c(new Date(2019, 0, 25, 9, 15).getTime(), '學測', new Date(2019, 0, 26, 16, 50).getTime());
 
-function c (time, text) {
+function c (time, text, end) {
     const target = document.createElement('h1');
     container.appendChild(target);
     count(time, text, target);
-    setInterval(count, 1000, time, text, target);
+    setInterval(count, 1000, time, text, target, end);
 }
 
-function count (time, text, target) {
+function count (time, text, target, end) {
     const now = new Date().getTime();
     if (now < time){
         const r = time - now;
@@ -22,13 +22,15 @@ function count (time, text, target) {
             minute = Math.floor(r % 3600000 / 60000),
             second = Math.floor(r % 60000 / 1000);
         target.innerHTML = `距離${text}還有 ${day}天 ${hour}小時 ${o(minute)}分鐘 ${o(second)}秒`;
-    } else {
+    } else if (now > end){
         const r = now - time;
         const day = Math.floor(r / 86400000),
             hour = Math.floor(r % 86400000 / 3600000),
             minute = Math.floor(r % 3600000 / 60000),
             second = Math.floor(r % 60000 / 1000);
         target.innerHTML = `距離${text}已過 ${day}天 ${hour}小時 ${o(minute)}分鐘 ${o(second)}秒`;
+    } else {
+        target.innerHTML = `${text}進行中`;
     }
 }
 
